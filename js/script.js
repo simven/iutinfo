b1d38f06492228acbc32ad3e057f9df6307651a2
simven <56834754+simven@users.noreply.github.com>
2019-12-22 22:57:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+jQuery(function () {
+    "use strict";
+    $(function () {
+        $(window).scroll(function () {  //Fonction pour déscendre la page
+            if ($(this).scrollTop() > 200) {  // A 200pixels du haut de page
+                $('#scrollUp').css('right', '10px');  // Replace l'image à 10pixels à droite
+            } else {
+                $('#scrollUp').removeAttr('style'); // Supprime les attributs CSS affectés par javascript
+            }
+        });
+    });
+
+    $("#scrollUp").click(function () {
+        $("html, body").animate({scrollTop: 0}, "slow");
+    });
+
+    $(document).ready(function () {
+        $('a[href^="#"]').on('click', function (evt) {
+            evt.preventDefault();
+            var target = $(this).attr('href');
+            $('html, body')
+                .stop()
+                .animate({scrollTop: $(target).offset().top -70}, 1000);
+        });
+    });
+});
